6d1615273c00096ab46bbcb87bda9fac09fc4e62
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-11-12 20:08:19 +0900

    bug fix

--- src/api/Ride/NearbyRideSubscription/NearbyRideSubscription.resolvers.ts
+++ src/api/Ride/NearbyRideSubscription/NearbyRideSubscription.resolvers.ts
@@ -1,4 +1,4 @@
-import { withFilter } from "../../../../node_modules/graphql-yoga";
+import { withFilter } from "graphql-yoga";
 import User from "../../../entities/User";
 
 const resolvers = {
@@ -6,27 +6,22 @@ const resolvers = {
     NearbyRideSubscription: {
       subscribe: withFilter(
         (_, __, { pubSub }) => pubSub.asyncIterator("rideRequest"),
-        (payload, _, { context }) => {
+        (payload, __, { context }) => {
           const user: User = context.currentUser;
           const {
-            NearbyRideSubscription: {
-              pickUpLat,
-              pickUpLng
-            }
+            NearbyRideSubscription: { pickUpLat, pickUpLng }
           } = payload;
-          const { lastLat: userLastLat, lastLng: userLastLng } = user;
+          const { lastLat: driverLastLat, lastLng: driverLastLng } = user;
           return (
-            pickUpLat >= userLastLat - 0.05 &&
-            pickUpLat <= userLastLat + 0.05 &&
-            pickUpLng >= userLastLng - 0.05 &&
-            pickUpLng <= userLastLng + 0.05
+            pickUpLat >= driverLastLat - 0.05 &&
+            pickUpLat <= driverLastLat + 0.05 &&
+            pickUpLng >= driverLastLng - 0.05 &&
+            pickUpLng <= driverLastLng + 0.05
           );
         }
       )
     }
   }
-}
+};
 
-
-
-export default resolvers
+export default resolvers;
