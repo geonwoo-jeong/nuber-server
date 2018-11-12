86ed5bc4d9077be9add77a44419ec4b880e67e16
Geonwoo-Jeong <geonwoo.jeong@gmail.com>
2018-11-12 22:16:18 +0900

    relations message -> messages

--- src/api/Chat/GetChat/GetChat.resolvers.ts
+++ src/api/Chat/GetChat/GetChat.resolvers.ts
@@ -1,56 +1,52 @@
 import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
-import { GetChatResponse } from "../../../types/graph";
+import { GetChatQueryArgs, GetChatResponse } from "../../../types/graph";
 import User from "../../../entities/User";
 import Chat from "../../../entities/Chat";
 
 const resolvers: Resolvers = {
- Query: {
-   GetChat: privateResolver(
-     async(
-       _,
-       args,
-       { req }
-     ): Promise<GetChatResponse> => {
-      const user : User = req.user;
-      try {
-        const chat = await Chat.findOne(
-          {
-          id: args.chatId
-          },
-          { relations: ["message"]}
-        )
-        if (chat) {
-          if (chat.passengerId === user.id || chat.driverId === user.id) {
-            return {
-              ok: true,
-              error: null,
-              chat
+  Query: {
+    GetChat: privateResolver(
+      async (_, args: GetChatQueryArgs, { req }): Promise<GetChatResponse> => {
+        const user: User = req.user;
+        try {
+          const chat = await Chat.findOne(
+            {
+              id: args.chatId
+            },
+            { relations: ["messages"] }
+          );
+          if (chat) {
+            if (chat.passengerId === user.id || chat.driverId === user.id) {
+              return {
+                ok: true,
+                error: null,
+                chat
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not Authorized",
+                chat: null
+              };
             }
           } else {
             return {
               ok: false,
-              error: "Not Authorized",
+              error: "Not found",
               chat: null
-            }
+            };
           }
-        } else {
+        } catch (error) {
           return {
             ok: false,
-            error: "Not found",
+            error: error.message,
             chat: null
-          }
+          };
         }
-      } catch(error) {
-        return {
-          ok: false,
-          error: error.message,
-          chat: null
-       }
       }
-     }
-   )
- }
-}
+    )
+  }
+};
 
 export default resolvers;
